20c5e5ad14ee3ea8eed63acfeba1c8ecd57cdf30
Carlos V <77943334+valdcarl@users.noreply.github.com>
2022-04-15 18:29:57 -0600

    Commit Changes Added 4/15/2022 --- Details in README with corresponding date --- Carlos A. Valdez

--- app/src/main/java/edu/cudenver/sudokuapplication/MainActivity.java
+++ app/src/main/java/edu/cudenver/sudokuapplication/MainActivity.java
@@ -11,13 +11,53 @@ package edu.cudenver.sudokuapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playBtn; // var to prepare playBtn
+    private Button exitBtn; // var to prepare exitBtn
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // must initialize the buttons play and exit
+        // each needs a setOnClickListener
+        playBtn = findViewById(R.id.playBtn);
+        exitBtn = findViewById(R.id.exitBtn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // makes a call to onClickPlayBtn function below which starts the activity
+                onClickPlayBtn();
+            }
+        });
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // makes a call to onClickExitBtn function below which force quits application
+                onClickExitBtn();
+            }
+        });
+    }
+
+
+    public void onClickPlayBtn() {
+        // When the user clicks on the play button
+        // We should go to the next activity that will display the difficulty levels
+        Intent intent = new Intent(this, SelectDifficultyActivity.class);
+        startActivity(intent);
+    }
+    public void onClickExitBtn() {
+        // When the user clicks on the exit button
+        // We should close the application completely
+        finish();
+        System.exit(0);
     }
 }

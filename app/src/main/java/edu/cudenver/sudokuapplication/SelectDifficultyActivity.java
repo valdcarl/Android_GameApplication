bbe0c06e6babeac80b52f9226449684a93f2d0f3
Carlos V <77943334+valdcarl@users.noreply.github.com>
2022-04-15 18:31:05 -0600

    Commit Changes Added 4/15/2022 --- Details in README with corresponding date --- Carlos A. Valdez

---
+++ app/src/main/java/edu/cudenver/sudokuapplication/SelectDifficultyActivity.java
@@ -0,0 +1,14 @@
+package edu.cudenver.sudokuapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectDifficultyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_difficulty);
+    }
+}
